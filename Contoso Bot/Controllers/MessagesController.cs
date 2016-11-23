b376dc9e708d226791794f2dc8bbb0370fcdd562
Aurora <Montacia@gmail.com>
2016-11-23 23:21:13 +1300

    dialogs not working

--- Contoso Bot/Controllers/MessagesController.cs
+++ Contoso Bot/Controllers/MessagesController.cs
@@ -9,9 +9,70 @@ using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
 using Microsoft.WindowsAzure.MobileServices;
 using System.Collections.Generic;
+using Microsoft.Bot.Builder.Dialogs;
+using Contoso_Bot.DataModels;
 
 namespace Contoso_Bot
 {
+    [Serializable]
+    public class loginDialog : IDialog<object>
+    {
+        protected string username;
+        protected string password;
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            PromptDialog.Text(
+                context,
+                storeuser,
+                "Please type in your Username."
+                );
+            PromptDialog.Text(
+                context,
+                storepass,
+                "Please type in your Password."
+                );
+            await context.PostAsync(this.username + this.password);
+            context.Wait(MessageReceivedAsync);
+        }
+        public async Task storeuser(IDialogContext context, IAwaitable<string> argument)
+        {
+            var username = await argument;
+
+            this.username = username;
+
+            context.Wait(MessageReceivedAsync);
+        }
+        public async Task storepass(IDialogContext context, IAwaitable<string> argument)
+        {
+            var password = await argument;
+
+            this.password = password;
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+    
+
+    [Serializable]
+    public class updateDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            
+            var message = await argument;
+            await context.PostAsync("You said: " + message.Text);
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+
     [BotAuthentication]
     public class MessagesController : ApiController
     {
@@ -24,89 +85,114 @@ namespace Contoso_Bot
             if (activity.Type == ActivityTypes.Message)
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                MobileServiceClient dbclient = AzureManager.AzureManagerInstance.AzureClient;
-                intent.RootObject rootObject;
-                HttpClient luisclient = new HttpClient();
-                string x = await luisclient.GetStringAsync(new Uri("https://api.projectoxford.ai/luis/v2.0/apps/0bc944f0-ba4d-4c3c-9588-7587eabcd1d8?subscription-key=fe392207fc69410cb8f52911ac8b4599&q="+ activity.Text));
-                rootObject = JsonConvert.DeserializeObject<intent.RootObject>(x);
-                string intent = rootObject.topScoringIntent.intent;
-                //intent: none
-                Activity reply = activity.CreateReply($"Sorry, I don't understand. Please rephrase or type 'help' to see what I can do for you.");
 
-                //intent: help
-                if (intent == "help")
+                StateClient stateClient = activity.GetStateClient();
+                BotData userData = await stateClient.BotState.GetUserDataAsync(activity.ChannelId, activity.From.Id);
+
+                if (activity.Text == "help")
                 {
-                    
+                    Activity reply = activity.CreateReply($"I can tell you about the current exchange rates, accounts, cards, kiwisaver, loans, mortgages and term deposits. \n I can also do this.");
+                    await connector.Conversations.ReplyToActivityAsync(reply);
                 }
-
-                //intent: greeting
-                if (intent == "greeting")
+                else
                 {
-                    reply = activity.CreateReply($"Hello, how may I help you today?");
-                }
+                    MobileServiceClient dbclient = AzureManager.AzureManagerInstance.AzureClient;
+                    intent.RootObject rootObject;
+                    HttpClient luisclient = new HttpClient();
+                    string x = await luisclient.GetStringAsync(new Uri("https://api.projectoxford.ai/luis/v2.0/apps/0bc944f0-ba4d-4c3c-9588-7587eabcd1d8?subscription-key=fe392207fc69410cb8f52911ac8b4599&q=" + activity.Text));
+                    rootObject = JsonConvert.DeserializeObject<intent.RootObject>(x);
+                    string intent = rootObject.topScoringIntent.intent;
+                    //intent: none
+                    Activity reply = activity.CreateReply($"Sorry, I don't understand. Please rephrase or type 'help' to see what I can do for you.");
 
-                //intent: query
-                if (intent == "query")
-                {
-                    List<intent.Entity> entities = rootObject.entities;
-                    string entity = entities[0].type;
-                    if (entity.Contains("exchangerate"))
-                    {
-                        reply = activity.CreateReply($"mystomachhurts");
-                    }
-                    if (entity.Contains("accounts"))
+                    //intent: help
+                    if (intent == "help")
                     {
 
                     }
-                    if (entity.Contains("cards"))
+
+                    //intent: greeting
+                    if (intent == "greeting")
+                    {
+                        reply = activity.CreateReply($"Hello, how may I help you today?");
+                    }
+
+                    //intent: query
+                    if (intent == "query")
+                    {
+                        List<intent.Entity> entities = rootObject.entities;
+                        string entity = entities[0].type;
+                        if (entity.Contains("exchangerate"))
+                        {
+                            reply = activity.CreateReply($"mystomachhurts");
+                        }
+                        if (entity.Contains("accounts"))
+                        {
+
+                        }
+                        if (entity.Contains("cards"))
+                        {
+
+                        }
+                        if (entity.Contains("kiwisaver"))
+                        {
+
+                        }
+                        if (entity.Contains("loans"))
+                        {
+
+                        }
+                        if (entity.Contains("mortgages"))
+                        {
+
+                        }
+                        if (entity.Contains("termdeposits"))
+                        {
+
+                        }
+                    }
+
+
+                    //intent: open
+                    if (intent == "open")
                     {
 
                     }
-                    if (entity.Contains("kiwisaver"))
+
+                    //intent: updateinfo
+                    if (intent == "updateinfo")
+                    {   
+                        if (userData.GetProperty<bool>("loggedin"))
+                        {
+                            await Conversation.SendAsync(activity, () => new updateDialog());
+                        }
+                        else
+                        {
+                            await Conversation.SendAsync(activity, () => new loginDialog());
+                            userData.SetProperty<bool>("loggedin", true);
+                            await Conversation.SendAsync(activity, () => new updateDialog());
+                        }
+                        
+                        reply=activity.CreateReply($"");
+                    }
+
+                    //intent: disablecard
+                    if (intent == "disablecard")
                     {
 
                     }
-                    if (entity.Contains("loans"))
+
+                    //intent: close
+                    if (intent == "close")
                     {
 
                     }
-                    if (entity.Contains("mortgages"))
-                    {
 
-                    }
-                    if (entity.Contains("termdeposits"))
-                    {
 
-                    }
+                    await connector.Conversations.ReplyToActivityAsync(reply);
                 }
 
-
-                //intent: open
-                if (intent == "open")
-                {
-
-                }
-
-                //intent: updateinfo
-                if (intent == "updateinfo")
-                {
-
-                }
-
-                //intent: disablecard
-                if (intent == "disablecard")
-                {
-
-                }
-
-                //intent: close
-                if (intent == "close")
-                {
-
-                }
-
-
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                
             }
             else
             {
